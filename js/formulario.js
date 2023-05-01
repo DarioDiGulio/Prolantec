2074639014570747785ab5105cb9a251e31554d3
Dario Di Gulio <dario.digulio@grupoesfera.com.ar>
2023-05-01 00:29:51 -0300

    Corrijo asincronismo en envio de email y refactor

--- js/formulario.js
+++ js/formulario.js
@@ -26,43 +26,50 @@ function isValid(name, email, subject, message) {
     return name.value && email.value && subject.value && message.value;
 }
 
+function getFormValidatedData(fieldName) {
+    const field = document.getElementsByName(fieldName)[0];
+    if (!field.value) showError(fieldName);
+    else hiddenError(fieldName);
+    return field;
+}
+
+function clearForm(name, email, subject, message) {
+    name.value = '';
+    email.value = '';
+    subject.value = '';
+    message.value = '';
+}
+
+function emailContentBy(name, email, message) {
+    return `<Mensaje enviado por ${name.value} con mail ${email.value}>\n${message.value}`
+}
+
 function submitContactForm() {
     showLoaderForm()
-    const name = document.getElementsByName('name')[0];
-    if (!name.value) showError('name');
-    else hiddenError('name');
-    const email = document.getElementsByName('email')[0];
-    if (!email.value) showError('email');
-    else hiddenError('email');
-    const subject = document.getElementsByName('subject')[0];
-    if (!subject.value) showError('subject');
-    else hiddenError('subject');
-    const message = document.getElementsByName('message')[0];
-    if (!message.value) showError('message');
-    else hiddenError('message');
+    const name = getFormValidatedData('name');
+    const email = getFormValidatedData('email');
+    const subject = getFormValidatedData('subject');
+    const message = getFormValidatedData('message');
     if (isValid(name, email, subject, message)) {
         postData('https://prolantec-mailsender.onrender.com/send', {
-            name: name.value,
-            email: email.value,
             subject: subject.value,
-            message: message.value
+            content: emailContentBy(name.value, email.value, message.value)
         }).then(() => {
-            name.value = '';
-            email.value = '';
-            subject.value = '';
-            message.value = '';
+            clearForm(name, email, subject, message)
+        }).catch((e) => {
+            console.error('No se pudo enviar el email.');
+            console.error(e);
         });
     }
-    hideLoaderForm();
+    else hideLoaderForm();
 }
 
 async function postData(url, data) {
-    const response = await fetch(url, {
+    await fetch(url, {
         method: "POST",
         cache: "no-cache",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
     });
-    const result = await response.json();
-    console.log(result);
+    hideLoaderForm();
 }
